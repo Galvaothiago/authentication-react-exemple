20ee01b1174f6a575f7fde5793570ba6c2978139
Thiago Galvao <thiagosilva128@gmail.com>
2022-01-18 00:48:32 -0300

    get user in localStarage

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -7,29 +7,46 @@ export const AuthContext = createContext({})
 
 export function AuthProvider({ children }) {
     const [ user, setUser ] = useState({})
-    const [ dataUser, setDateUser ] = useLocalStorage("userInfo", undefined)
+    const [ tokenStorage,  setTokenStorage] = useLocalStorage("gasstationToken", undefined)
+    const [ refreshTokenStorage, setRefreshTokenStorage ] = useLocalStorage("gasstationRefreshToken", undefined)
 
     useEffect(() =>{
-            if(dataUser) {
-                setUser(dataUser)
+        const thereTokenAndRefreshToken = !!tokenStorage && !!refreshTokenStorage
+
+        const handleSetUser = async () => {
+            if(thereTokenAndRefreshToken) {
+                const userInfo = await api.get("/me")
+        
+                setUser({
+                    id: "test"
+                })
+                console.log(userInfo)
                 return
             }
             setUser(null)
-    }, [dataUser])
+        }
+
+        return () => handleSetUser()
+    }, [])
 
     const signIn = async (username, password) => {
         try {
             const data = await api.post('signin', { username, password})
-            const { id, accessToken, email, roles, username: userName } = data.data
+            const { id, accessToken, email, roles, refreshToken, username: userName } = data.data
             
-            setDateUser({ 
+            setUser({ 
                 id,
                 userName,
                 email,
-                accessToken,
                 roles
             })
-            // setUser(dataUser)
+
+            setTokenStorage(accessToken)
+            setRefreshTokenStorage(refreshToken)
+
+            api.defaults.headers['Authorization'] = `Bearer ${accessToken}`
+            console.log(accessToken)
+
         } catch(error) {
             console.log("error:", error)
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+const token = localStorage.getItem('gasstationToken').replace(/"/g, "")
+
 export const api = axios.create({
-    baseURL: 'http://localhost:8080/api/auth'
+    baseURL: 'http://localhost:8080/api/auth',
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
 })
